e5723ea8172d125e14abc3c6dbc3b06bbb3b279d
cn-kali-team <cn-kali-team@users.noreply.github.com>
2025-12-01 14:09:17 +0000

    deploy: eacfba33cfe7e99f6ad47ba2489ffe98fea9d6f2

---
+++ trait.impl/serde_core/de/trait.Deserialize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["asynq",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.Deserialize.html\" title=\"trait serde_core::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"enum\" href=\"asynq/base/keys/enum.TaskState.html\" title=\"enum asynq::base::keys::TaskState\">TaskState</a>"],["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.Deserialize.html\" title=\"trait serde_core::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"asynq/task/struct.DailyStats.html\" title=\"struct asynq::task::DailyStats\">DailyStats</a>"],["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.Deserialize.html\" title=\"trait serde_core::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"asynq/task/struct.QueueInfo.html\" title=\"struct asynq::task::QueueInfo\">QueueInfo</a>"],["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.Deserialize.html\" title=\"trait serde_core::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"asynq/task/struct.QueueStats.html\" title=\"struct asynq::task::QueueStats\">QueueStats</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1266]}
